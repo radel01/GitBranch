f6aaee07593f96166610b6e7d23250bfd3227de7
roszkopf.a.lena <roszkopf.a.lena@host.docker.internal>
2024-09-30 13:36:34 +0200

    merge

--- src/gitbranch/GitBranch.java
+++ src/gitbranch/GitBranch.java
@@ -4,12 +4,15 @@ package gitbranch;
 public class GitBranch {
 
     public static void main(String[] args) {
-        int i= GitBranch.fv();
+        GitBranch obj = new GitBranch();
+        int i= obj.fv();
     }
 
-    private static int fv() {
-        System.out.println("static fv");
-        return -1;
+    private int fv() {
+        System.out.println("nem static fv");
+        return 0;
     }
-    
+    public void elj(){
+        
+    }
 }
