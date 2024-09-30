f2775ac357cf30b22727e9fad29fd9418463d12b
roszkopf.a.lena <roszkopf.a.lena@host.docker.internal>
2024-09-30 13:17:23 +0200

    fv fuggveny

--- src/gitbranch/GitBranch.java
+++ src/gitbranch/GitBranch.java
@@ -4,6 +4,11 @@ package gitbranch;
 public class GitBranch {
 
     public static void main(String[] args) {
+        int i= fv();
+    }
+
+    private static int fv() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
